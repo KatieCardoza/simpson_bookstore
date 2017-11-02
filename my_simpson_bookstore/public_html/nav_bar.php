fb10df70a27a66a07beb267303c63bc8a173d66d
SC\Katie.cardoza <katie.cardoza@my.simpson.edu>
2017-11-02 09:32:19 -0500

    Created nav php, no net changes to sign up, disregard "nav_bar.php"

---
+++ my_simpson_bookstore/public_html/nav_bar.php
@@ -0,0 +1,35 @@
+<!-- Navigation bar-->
+
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+
+            <!-- Three line "hamburger" icon for the collapsed navigation -->
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+
+          <!-- Title on nav bar -->
+          <a class="navbar-brand" href="#">Bootstrap</a>
+        </div>
+
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+      <li><a href="index.html">Home</a></li>
+      <li><a href="sign_up.html">Sign Up</a></li>
+      <li><a href="login.html">Login</a></li>
+      <li><a href="office_basics.html">Office Basics</a></li>
+      <li><a href="notebooks.html">Notebooks</a></li>
+      <li><a href="writing_supplies.html">Writing Supplies</a></li>
+      <li><a href="calendars_and_planners.html">Calendars and Planners</a></li>
+      <li><a href="on_the_go.html">On The Go</a></li>
+      <li><a href="about.html">About</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    
